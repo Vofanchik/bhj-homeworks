f6ac30391b2f56a3cfc649ec2162de971ca16b0d
Vladimir Kalinin <Vofamchic@mail.ru>
2023-11-29 23:44:21 +0300

    tooltip fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,15 +4,23 @@ tooltip.classList.add("tooltip")
 
 for (let item of hasTooltip) {
     item.addEventListener("click", (event) => {
+        event.preventDefault()
         let position = item.getBoundingClientRect()
 
-        event.preventDefault()
-        item.insertAdjacentElement("afterend", tooltip)
+        let openedTooltips = document.querySelectorAll('.tooltip')
+        if (openedTooltips.length > 1) {
+            openedTooltips[0].remove()}
+                      
+        let tooltipLinkActive = item.querySelector('.tooltip_active');
+        if (tooltipLinkActive) {
+            tooltipLinkActive.remove()            
+        }else{
+        item.insertAdjacentElement("beforeend", tooltip)
         let attr = item.getAttribute("title")
         tooltip.textContent = attr
-        tooltip.classList.toggle("tooltip_active")
+        tooltip.classList.add("tooltip_active")
 
-        tooltip.style.top = position.bottom + 'px';
-        tooltip.style.left = position.left + 'px';
+        tooltip.style.top = position.bottom + 'px'
+        tooltip.style.left = position.left + 'px'}
     })
 }
